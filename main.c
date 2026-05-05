6f5d4d4ccb1b688fe0edcbd0ccc501e3f2321103
Nadir.Dahlgren@outlook.com <Nadir.Dahlgren@outlook.com>
2026-05-05 18:45:22 +0200

    Finale

--- main.c
+++ main.c
@@ -5,10 +5,30 @@ int main()
 {
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
+    int tärninglista[100]; // Array för tärningkast, 100st.
+    int sum = 0; // Summan för alla kast. 
+    float medelv = 0; // Medelvärdet av alla kast.
+    int one = 0, two = 0, three = 0, four = 0, five = 0, six = 0; 
 
-    // Test
-   
-    // Your code goes here
+    for (int i = 0; i < 100; i++)
+    {
+        int dice_roll = (rand() % 6) + 1; 
+        tärninglista[i] = dice_roll;
+        
+        if (dice_roll == 1) { one++; }
+        else if (dice_roll == 2) { two++; }
+        else if (dice_roll == 3) { three++; }
+        else if (dice_roll == 4) { four++; }
+        else if (dice_roll == 5) { five++; }
+        else if (dice_roll == 6) { six++; }
+        
+        // Addera varje kast till summan för att få totalen (326)
+        sum += dice_roll;
+    }
+    
+    medelv = (float)sum / 100;
+
+    printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%.1f\n", one, two, three, four, five, six, sum, medelv); 
 
     return 0;
 }
